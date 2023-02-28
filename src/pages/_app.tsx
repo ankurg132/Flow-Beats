c72d3741f06253d6d54f2ece646af6f6251949d2
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-02-28 22:28:44 +0530

    the problom in favicon is fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,8 +47,8 @@ const App = ({ Component, pageProps: { session, auth, ...pageProps } }: AppProps
       <Head>
         <title>{DEFAULT_TITLE}</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.ico" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#ffffff" />
