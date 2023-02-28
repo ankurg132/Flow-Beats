6793232eb690079de09ef16dfafec8a419aba1d2
Ankur Gupta <ankurg052@gmail.com>
2023-03-01 04:03:57 +0530

    merge conflict 1 solved

--- src/pages/app/account.tsx
+++ src/pages/app/account.tsx
@@ -26,18 +26,7 @@ const AccountPage = () => {
 
   const fetching = walletFetching || isLoading
   return (
-<<<<<<< HEAD
-    <AppLayout title="Account ">
-
-      <Flex justifyContent="center" alignItems="center">
-        <Image src={dashboard} alt="dashboard" />
-      </Flex> 
-
-
-=======
-    
     <AppLayout title="Account | Flow Beats ">
->>>>>>> 2823bb03f4b58a4bd155d8f11f121f03a8c92fe9
       <Flex
         gap="10"
         textAlign="center"
